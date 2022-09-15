d86e33ff42a39e6e42fe3c5631e14137cbcc093b
Nina Palma <ninapalmadev@gmail.com>
2022-09-15 11:26:41 -0300

    cria repositório

---
+++ src/exercicio1/Main.java
@@ -0,0 +1,50 @@
+package exercicio1;
+
+//Aula 1 - variáveis, Tipos de dados e operadores
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+       // int i;
+        int I;
+        //int 1x;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn3293nd;
+        //int asrn329 3nd;
+        int asrn3$293_nd = 12;
+        //int asrn32%93_nd = 15;
+
+        asrn3293nd = 13;
+        asrn3$293_nd = 200;
+
+        int quantidadeProduto = 40;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn3293nd);
+        System.out.println(asrn3$293_nd);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
